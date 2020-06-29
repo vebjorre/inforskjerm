a55df9be9bd4039d18612b8f64431152e98674ae
Vebjørn Rekkebo <vebjorn@Vebjrns-MacBook-Pro.local>
2020-06-29 13:34:01 +0200

    initial commit

---
+++ src/Bikes.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect} from "react";
+import createEnturService from '@entur/sdk';
+
+
+const service = createEnturService({ clientName: 'superbur-inforskjermkurs' });
+
+const Bikes = () => {
+    const [bikeStations, setBikeStations] = useState([]);
+    useEffect(() => {
+        service.getBikeRentalStationsByPosition({latitude: 59.9289553, longitude: 10.7586829},
+            500
+            )
+            .then((data) => setBikeStations(data));
+    }, []);
+
+    console.log(bikeStations);
+    return <div className="BikeStations">
+        <h3>Bysykkel</h3>
+        {bikeStations.map((stationData)  => (
+            <Station key={stationData.id} station={stationData} />
+        ))}
+        </div>;
+};
+
+export default Bikes;
+
+
+const Station = (props) => {
+    const { station } = props;
+
+    console.log(station);
+    return <div className="station">
+        {station.name} - bikes: {station.bikesAvailable}, spaces: {station.spacesAvailable}.
+        </div>
+};
